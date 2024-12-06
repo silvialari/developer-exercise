c2c805d4ebe48f5a72fbd922d78e780549e360f1
Silvia Lari <silvialari@Mac.hsd1.ca.comcast.net>
2024-12-05 17:08:17 -0800

    Answers to exercise

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,18 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    return_str = ""
+    for word in str.split()   
+       if word.length > 4
+           if word[0] == word[0].capitalize
+               word = word.gsub(/[A-Za-z]+/, "Marklar")
+           else
+               word = word.gsub(/[A-Za-z]+/, "marklar")
+           end
+       end
+       return_str += word + " "
+    end
+    return_str.strip
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -12,7 +23,17 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
-  end
+    # next two lines from:
+    #    https://stackoverflow.com/questions/12178642/fibonacci-sequence-in-ruby-recursion#:~:text=2)%20Example%2C%20where%2010%20elements
+    fib = [0, 1]
+    (2..nth-1).each { fib << fib[-1] + fib[-2] }
 
+    sum = 0
+    for n in fib
+       if n % 2 == 0
+          sum += n
+       end 
+    end
+    return sum
+  end
 end
